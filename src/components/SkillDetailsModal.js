664bbec7c7e6ba87b4cf615a4e6f827cdf269ac8
sparshhgupta <sparsh105gupta@gmail.com>
2025-08-30 23:27:39 +0530

    removed links from skill modal and minor data changes

--- src/components/SkillDetailsModal.js
+++ src/components/SkillDetailsModal.js
@@ -87,7 +87,7 @@ const SkillDetailsModal = ({ skill, onClose, projects, experiences }) => {
                       </div>
                       <h3 className="text-lg font-bold mb-2">{project.title}</h3>
                       <p className="text-gray-600 text-sm mb-3">{project.description}</p>
-                      <div className="flex space-x-2">
+                      {/* <div className="flex space-x-2">
                         <button className="flex items-center space-x-1 text-blue-600 hover:text-blue-700 text-sm">
                           <Github size={14} />
                           <span>Code</span>
@@ -96,7 +96,7 @@ const SkillDetailsModal = ({ skill, onClose, projects, experiences }) => {
                           <ExternalLink size={14} />
                           <span>Demo</span>
                         </button>
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 ))}
